dd978388db4167edc0dcddb92e38e3634ef1d339
Theusboot <emailmartingcf1@gmail.com>
2023-09-21 02:15:39 -0300

    servidor 1

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uVicorn
 
 app = FastAPI()
+
+
+class Inputs(BaseModel):
+    inp: int
+    inpt2: str
+
 
 
 @app.get("/exemplo")
@@ -9,6 +17,9 @@ def exemplo() -> str:
     #status_code == 200
     #result = "hello mundo"
 
+@app.post("/exemplo_2")
+def exemplo_2(inputs:Inputs) -> str:
+    return inputs.inpt2
 
 
 if __name__ == "__main__":
