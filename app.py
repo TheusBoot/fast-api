c8c43db62f98f1806fe0cec01bc43904e71d3477
Theusboot <emailmartingcf1@gmail.com>
2023-09-21 02:09:27 -0300

    api iniciada

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/exemplo")
+def exemplo() -> str:
+    return "hello mundo"
+    #status_code == 200
+    #result = "hello mundo"
+
+
+
+if __name__ == "__main__":
+    uVicorn.run(app, port=8000)
